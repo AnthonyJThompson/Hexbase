b2b38838c10e634f0cef2aadd6679fd5560fd579
AnthonyJThompson <tehcoolzor@gmail.com>
2018-01-20 13:31:21 -0600

    Initial commit

---
+++ Assets/Scripts/Shop/Purchase.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Purchase : MonoBehaviour {
+
+	public HexGrid hexGrid;
+	public GameObject FarmPrefab;
+	GameObject selectedObject;
+
+	void Awake () {
+		
+	}
+
+	void Update () {
+		HandleInput();
+	}
+
+	void HandleInput () {
+		Ray inputRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		HexCell cell = null;
+		if (selectedObject != null) {
+			bool snapped = false;
+			if (Physics.Raycast(inputRay, out hit)) {
+				cell = hexGrid.GetCellFromPosition(hit.point);
+				if (cell != null) {
+					selectedObject.SetActive(true);
+					selectedObject.transform.position = cell.transform.position;
+					Debug.Log(cell.transform.position);
+					snapped = true;
+				}
+			}
+			if (!snapped) {
+				selectedObject.SetActive(false);
+			}
+		}
+		if (Input.GetMouseButton(0) && cell != null) {
+			selectedObject = null;
+		}
+	}
+
+	public void SelectFarm () {
+		Destroy(selectedObject);
+		selectedObject = Instantiate<GameObject>(FarmPrefab);
+		selectedObject.SetActive(false);
+	}
+}
